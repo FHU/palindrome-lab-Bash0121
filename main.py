15b664c1a232ca309924807eb009f3170c6ff73a
Bash0121 <Robert.smith@students.fhu.edu>
2024-02-06 14:13:17 -0600

    update main.py
    update 1

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 def palindrome(word):
-    pass
+    list2=word[:]
+    reversed_list=reversed(list2)
 
+    joined_list1="".join(word)
+    joined_list2="".join(reversed_list)
+    
+    if joined_list1==joined_list2:
+        return True
+    else:
+        return False                 
 if __name__ == '__main__': 
     #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+    u_input=input()
+    if u_input=='" "':
+        print("How")
+    else:
+        u_list=list(u_input)
+        print(palindrome(u_list))
+        
+#take the u_input list, copy it and flip it in palindrome, then compare the two. If they are palindromes than they will equal True. 
+#If not they will be false, whitespace should not count.
